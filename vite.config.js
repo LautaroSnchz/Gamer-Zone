8a350c2eed359579abf69c94f688c2ac52161a22
LautaroSnchz <lautaro.snchz94@gmail.com>
2024-12-11 15:14:19 -0300

    ``

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Gamer-Zone/', // Nombre de tu repositorio
+  base: '/Gamer-Zone/',  
 })
